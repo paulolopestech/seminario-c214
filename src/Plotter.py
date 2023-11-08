c4fc3e37fee46a8ef67ffedce19d299a6c9ab5f8
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-11-08 20:27:07 -0300

    tests :construction:

--- src/Plotter.py
+++ src/Plotter.py
@@ -3,22 +3,22 @@ import seaborn as sb
 
 class Plotter:
     
-    def _init_(self):        
+    def init(self):        
         pass
 
-    @staticmethod
-    def plot(data):
-        output_directory = './buffer'
+    def plot(self, data):
+        output_directory = './buffer/'
         if data  == -1:
             plt.legend(loc='center')
             plt.plot(1, label='ERRO')
-            plt.savefig('buffer/erro_plot.png', format='png')
+            plt.savefig('./buffer/erro_plot.png', format='png')
             output_file = output_directory + 'data_plot.png'
+            plt.close()
         else:
             plt.legend(loc='center')
             plt.plot(data)
             output_file = 'buffer/data_plot.png'
-            plt.savefig('buffer/data_plot.png', format='png')
+            plt.savefig('./buffer/data_plot.png', format='png')
             output_file = output_directory + 'data_plot.png'
             plt.close()
 
