f1e5a4da382f3fe80ce17d1b7d3789eed7decd05
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-11-08 20:06:06 -0300

    tests

--- src/Plotter.py
+++ src/Plotter.py
@@ -7,16 +7,18 @@ class Plotter:
         pass
 
     def plot(self, data):
-        output_file = 'file.png'
+        output_directory = './buffer'
         if data  == -1:
             plt.legend(loc='center')
             plt.plot(1, label='ERRO')
             plt.savefig('buffer/erro_plot.png', format='png')
+            output_file = output_directory + 'data_plot.png'
         else:
             plt.legend(loc='center')
             plt.plot(data)
             output_file = 'buffer/data_plot.png'
-            plt.savefig(output_file, format='png')
+            plt.savefig('buffer/data_plot.png', format='png')
+            output_file = output_directory + 'data_plot.png'
             plt.close()
 
         return output_file
