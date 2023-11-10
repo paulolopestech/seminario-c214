6e5f5dbec7c360e96c6c73e320f42ae15ecc25c5
paulolopestech <phlj.junior@gmail.com>
2023-11-10 06:29:07 -0300

    :construction: inject flask in WebServer api instance

--- src/api.py
+++ src/api.py
@@ -1,8 +1,10 @@
 from WebServer import WebServer
 from Plotter import Plotter
+from flask import Flask, send_file, request
+
 
 if __name__ == '__main__':
     plotter = Plotter()
-    ws = WebServer()
+    ws = WebServer(request, Flask(__name__))
     ws.add_route_execute('/plot/line/', plotter)
     ws.run_server()
